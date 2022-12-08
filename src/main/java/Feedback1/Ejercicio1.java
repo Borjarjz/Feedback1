63a031e5bbbe39520e3e0e7649ac7376638ccea9
Borja Rodriguez <brodrjim@myuax.com>
2022-12-08 12:51:30 +0100

    Cambios en Ej 2

---
+++ src/main/java/Feedback1/Ejercicio1.java
@@ -0,0 +1,77 @@
+package Feedback1;
+/*
+*   Realiza una aplicación en la que se muestre, mediante una barra de progreso y una etiqueta de texto, la cuenta atrás desde una cantidad de segundos introducida por el usuario. En cualquier momento éste podrá cancelar la cuenta:
+*       1 La tarea se lanzará en segundo plano una vez el usuario pulse el botón
+*       2 La tarea mantendrá la cuenta atrás de los segundos que el usuario haya introducido
+*       3 El usuario podrá cancelar la tarea en cualquier momento
+*       4 Se mostrará el progreso en una barra de progreso y como texto en una etiqueta
+*       5 Cuando la tarea termine mostrará un mensaje en una ventana emergente
+*
+*      BORJA RODRIGUEZ JIMENEZ
+* */
+
+
+
+
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Ejercicio1 implements ActionListener,Runnable{
+
+    public Ejercicio1() {//constructor
+
+
+
+        JFrame frame =new JFrame("Ejercicio1");
+        JPanel panel=new JPanel();
+        JButton button1=new JButton("Cancelar");
+
+
+
+        button1.addActionListener(this);
+        panel.setBorder(BorderFactory.createEmptyBorder(300,300,100,300));
+        panel.setLayout(new GridLayout(0,1));
+        panel.add(button1);
+        frame.add(panel,BorderLayout.CENTER);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        new Ejercicio1();
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    @Override
+    public void run() {
+
+    }
+
+
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        //ACCION DE CANCELAR
+    }
+}
