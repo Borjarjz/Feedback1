c717729fdc2ba2ceb3ea91de714e212f4d243a87
Borja Rodriguez <brodrjim@myuax.com>
2022-12-08 12:28:07 +0100

    Cambios en Ej 2

--- src/main/java/Feedback1/Ejercicio2.java
+++ src/main/java/Feedback1/Ejercicio2.java
@@ -26,7 +26,7 @@ public class Ejercicio2 implements Runnable{
 
         //iniciamos los dos hilos
         hilo1.start();
-        hilo1.sleep(100);// llamamos al método sleep del hilo 1 durante 100ms para asegurarnos de que el hilo2 se lanzará mientras se sigue ejecutando el hilo1
+        Thread.sleep(100);// llamamos al método sleep del hilo 1 durante 100ms para asegurarnos de que el hilo2 se lanzará mientras se sigue ejecutando el hilo1
         hilo2.start();
 
 
